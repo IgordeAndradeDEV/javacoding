ab1cf328769bcbdf36045469307f401bfcdd084f
Igor <igor_6500@hotmail.com>
2025-06-09 18:44:14 -0300

    first commit

---
+++ src/test/java/com/igor/javacoding/JavacodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.javacoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
